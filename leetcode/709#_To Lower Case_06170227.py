863beaa813a336506e40e257f4d52a99b1196e95
angelawu0113 <55569849+angelawu0113@users.noreply.github.com>
2020-01-09 16:36:42 +0800

    Add files via upload

---
+++ leetcode/709#_To Lower Case_06170227.py
@@ -0,0 +1,18 @@
+
+# coding: utf-8
+
+# In[4]:
+
+
+class Solution:
+    def toLowerCase(self, str: str) -> str:
+        str = str.lower()
+        return str
+
+
+# In[5]:
+
+
+str = "HELlo"
+Solution().toLowerCase(str)
+
